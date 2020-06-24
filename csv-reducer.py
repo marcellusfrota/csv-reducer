5ad2253de19c558b4ed1cfb520ba2c0456d95a63
Marcellus Frota <marcellus@csktech.com.br>
2020-06-24 00:21:15 -0300

    fix: list of duplicated cols

--- csv-reducer.py
+++ csv-reducer.py
@@ -15,7 +15,7 @@ def main():
 
 	clear()
 
-	title_perm = 'CSV Reducer 0.1 - by Marcellus Frota'
+	title_perm = 'CSV Reducer 0.1'
 
 	parser = argparse.ArgumentParser(description=title_perm, epilog='let\'s reduce!')
 
@@ -31,13 +31,16 @@ def main():
 	parser.add_argument('-s', '--csv-separator', type=str, default=',', 
 		dest='csv_separator', help='Caracter used to separate columns in CSV input/output file')
 
+	parser.add_argument('-e', '--csv-encoding', type=str, default='iso-8859-1', 
+		dest='csv_encoding', help='CSV encoding type, default is iso-8859-1')
+
 	args = parser.parse_args()
 
 	selected_cols = None
 
 	try:
 
-		df_header = pd.read_csv(args.input_file, encoding='iso-8859-1', nrows=1, sep=args.csv_separator) # , usecols=['Nome', 'E-mail', 'CPF', 'Segmetacao']
+		df_header = pd.read_csv(args.input_file, encoding=args.csv_encoding, nrows=1, sep=args.csv_separator) # , usecols=['Nome', 'E-mail', 'CPF', 'Segmetacao']
  
 		title = title_perm + '\n\nChoose columns to be used (press SPACE to mark, ENTER to continue): '
 		options = df_header.columns.__dict__['_data']
@@ -47,21 +50,23 @@ def main():
 	except FileNotFoundError:
 		print(":ERROR - CSV Input file do not exist.")
 
-	df = pd.read_csv(args.input_file, encoding='iso-8859-1', usecols=selected_cols)
+	if (args.remove_duplicate):
+		duplicate_title = 'Choose columns to be checked for duplicates (press SPACE to mark, ENTER to continue): '
+		duplicate_selected = pick(selected_cols, duplicate_title, multiselect=True, min_selection_count=1)
+		duplicate_selected_cols = [d[0] for d in duplicate_selected]
 
 	print(title_perm + "\n")
 
+	try:
+		df = pd.read_csv(args.input_file, encoding=args.csv_encoding, sep=args.csv_separator, usecols=selected_cols)
+	except ValueError as error:
+		print(":ERROR", error)
+
 	if (args.remove_duplicate):
-
-		duplicate_title = 'Choose columns to be checked for duplicates (press SPACE to mark, ENTER to continue): '
-		duplicate_selected = pick(options, duplicate_title, multiselect=True, min_selection_count=1)
-		duplicate_selected_cols = [d[0] for d in duplicate_selected]		
-
 		df.drop_duplicates(subset=duplicate_selected_cols, keep='first', inplace=True)
-
 		print(":Duplicates removed!")
 
-	df.to_csv(args.output_file, index=False, sep=args.csv_separator)
+	df.to_csv(args.output_file, encoding=args.csv_encoding, index=False, sep=args.csv_separator)
 
 	print(":Done! have fun :)\n")
 	print("--- end of execution in %s seconds ---" % (time.time() - start_time))
